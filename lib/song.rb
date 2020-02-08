af08c1f8926bf3447dba738965b954ca4681f5b9
James Rogers <jarogers095@gmail.com>
2020-02-08 07:10:49 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  attr_reader(:count, :artists, :genres)
+  attr_accessor(:name, :artist, :genre)
+  
+  def self.count
+    return @@count
+  end
+  
+  def self.artists
+    return @@artists
+  end
+  
+  def self.genres
+    return @@genres
+  end
+  
+  def self.genre_count
+    return @@genres.size
+  end
+  
+  def self.artist_count
+    return @@artists.size
+  end
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    if !@@artists.include?(artist)
+      @@artists << artist
+    end
+    @@genres << genre
+  end
+end
