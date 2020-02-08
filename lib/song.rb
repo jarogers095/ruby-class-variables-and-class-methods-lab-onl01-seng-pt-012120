58a2797c386785a5a03a93a9fef4a77a141f597c
James Rogers <jarogers095@gmail.com>
2020-02-08 07:32:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
   end
   
   def self.artists
-    return @@artists.reduce(Array.new) do |uniques, artist|
+    return @@artists.reduce(Array.new(0)) do |uniques, artist|
       if !uniques.include?(artist)
         uniques << artist
       end
